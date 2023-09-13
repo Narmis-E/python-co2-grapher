5bd0d33aeeca346873224f16178e151f99377086
Narmis-E <narmisecurb@gmail.com>
2023-09-13 14:31:43 +0930

    added fox for years in decimal

--- test.py
+++ test.py
@@ -6,14 +6,14 @@ import matplotlib.pyplot as plt
 def main():
     # Load the CO2 emissions dataset using Pandas
     dataset = pd.read_csv("owid-co2-data.csv")
-    
+
     # Output a welcome message
     print("Welcome to the CO2 Emissions Visualization Program!")
     unqiue_country = dataset['country'].unique()
 
     # Prompt the user to input the desired country, start year, and end year
     country = input("Enter the country: ")
-    
+
     while country not in unqiue_country:
         print(f"{country} is not in the dataset\n")
         country = input("Enter the country again: ")
@@ -27,6 +27,9 @@ def main():
     # Filter the dataset for the selected country and years
     filtered_data = dataset[dataset['country'] == country]
     filtered_data = filtered_data[(filtered_data['year'] >= start_year) & (filtered_data['year'] <= end_year)]
+
+    # Convert the 'year' column to integers and then to strings
+    filtered_data['year'] = filtered_data['year'].astype("int").astype("str")
 
     # Create a basic line chart using Matplotlib to visualize CO2 emissions over time
     plt.plot(filtered_data['year'], filtered_data['co2'])
