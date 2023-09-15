7d64d74a93de077d2bb9263f94bcbd2b1bafe991
Narmis-E <narmisecurb@gmail.com>
2023-09-15 10:08:00 +0930

    removed main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import pandas as pd
-data=pd.read_csv('/home/narmis/Documents/owid-co2-data.csv')
-data_clean = data[(data['co2'] > 0) & (data['country'] != 'World')]
-
-print(data)
