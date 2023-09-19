ad47a91b2d2775ebd439321dcfeafa127c1b0a21
Narmis-E <narmisecurb@gmail.com>
2023-09-19 09:41:00 +0930

    added functions and graph selection

--- main.py
+++ main.py
@@ -2,64 +2,72 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Define the main function
-def main():
-    # Load the CO2 emissions dataset using Pandas
-    dataset = pd.read_csv("owid-co2-data.csv")
+def welcome_message():
+    print("CO2 Emissions Visualization Program")
+    print("-----------------------------------")
 
-    # Output a welcome message
-    print("Welcome to the CO2 Emissions Visualization Program!")
-    unqiue_country = dataset['country'].unique()
+def get_country(dataset):
+    unique_countries = dataset['country'].unique()
+    while True:
+        country = input("Enter a country (first letter caps): ")
+        if country in unique_countries:
+            return country
+        else:
+            print(f"{country} is not in the dataset\n")
 
-    # Prompt the user to input the desired country, start year, and end year
-    country = input("Enter the country: ")
-
-    while country not in unqiue_country:
-        print(f"{country} is not in the dataset\n")
-        country = input("Enter the country again: ")
-    
-    # Set valid years equal to the years each country has
+def get_years(dataset, country):
     valid_years = dataset[dataset['country'] == country]['year']
-    
-    # Get min and max years from valid_years
     min_year = int(valid_years.min())
     max_year = int(valid_years.max())
-
-    while country:
-        start_year = 0
-        end_year = 0
+    
+    while True:
         start_year = int(input(f"Enter the start year (min {min_year}): "))
-        if (start_year < min_year or start_year > max_year): 
-            print(f"{start_year} is not in the dataset\n")
+        if start_year < min_year or start_year > max_year:
+            print(f"{start_year} is not in the dataset or out of range\n")
             continue
 
         end_year = int(input(f"Enter the end year (max {max_year}): "))
-        if (end_year <= start_year):
-            print("End year must be greater than start year.")
+        if end_year <= start_year or end_year > max_year:
+            print("End year must be greater than start year and within the dataset's range.\n")
             continue
+        return start_year, end_year
 
-        if (end_year > max_year or end_year < min_year):
-            print(f"{end_year} is not in the dataset\n")
-            continue
-        break    
-
-    # Filter the dataset for the selected country and years
+def create_plot(dataset, country, start_year, end_year):
     filtered_data = dataset[dataset['country'] == country]
     filtered_data = filtered_data[(filtered_data['year'] >= start_year) & (filtered_data['year'] <= end_year)]
-
-    # Convert the 'year' column to integers and then to strings
     filtered_data['year'] = filtered_data['year'].astype("int")
 
-    # Create a basic line chart using Matplotlib to visualize CO2 emissions over time
-    plt.plot(filtered_data['year'], filtered_data['co2'])
-
-    # Customize the chart (e.g., labels, titles, legends)
+    print("-------------------------------")
+    print("Line: 1")
+    print("Scatter: 2")
+    print("Column: 3")
+    graph_choice = int(input("Please select a type of graph: "))
+    match graph_choice:
+        case 1:
+            plt.plot(filtered_data['year'], filtered_data['co2'], marker='.', markersize=15)
+        case 2:
+            plt.scatter(filtered_data['year'], filtered_data['co2'], s=100)
+        case 3:
+            plt.bar(filtered_data['year'], filtered_data['co2'])
+            
     plt.xlabel("Time (Year)")
     plt.ylabel("CO2 Emissions (Million Tons)")
     plt.title(f"{country} CO2 Emissions ({start_year}-{end_year})")
-
-    # Show the chart
+    figure = plt.gcf()
     plt.show()
+    plt.close()
+    save_choice = input("Do you wish to save the graph as an image? [Y/n]: ")
+    if (save_choice == 'y' or save_choice == ''):
+        figure_name = input("Enter a name for your graph: ")
+        figure.savefig(f'{figure_name}.png')
+    else:
+        print("Exiting")
 
-# Execute the main function
+def main():
+    dataset = pd.read_csv("owid-co2-data.csv")
+    welcome_message()
+    country = get_country(dataset)
+    start_year, end_year = get_years(dataset, country)
+    create_plot(dataset, country, start_year, end_year)
+
 main()
